11af94725b359d42ad3b519fa799b045b7074965
Ajay <majay1638@gmail.com>
2022-12-25 22:36:28 +0530

    changed url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-chatgpt-o33z.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
